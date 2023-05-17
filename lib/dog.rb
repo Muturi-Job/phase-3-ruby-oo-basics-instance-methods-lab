60a17ac933a364d5fdc98a3b4cdd6efc678bfbb0
Job Muturi <muirimuturi17@gmail.com>
2023-05-17 14:48:47 +0300

    Completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,14 +7,6 @@ class Dog
     end
 end
 
-class Person
-    def talk
-        puts "Hello World!"
-    end
-    def walk
-        puts "The Person is walking"
-    end
-end
 
 fido = Dog.new
 fido.bark
